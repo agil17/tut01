b2c1dccb4910c48048336eb55b9f7acd27b5deb6
agil17 <agil17@calstatela.edu>
2022-12-17 20:10:45 -0800

    Add Chapter 7 Code

---
+++ tutorial/src/Content.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+
+const Content = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "bag of flour"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "milk"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "almonds"
+    }
+  ]);
+
+  return (
+    <main>
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                checked={item.checked}
+              />
+              <label>{item.item}</label>
+              <FaTrashAlt role="button" tabIndex="0" />
+            </li>
+          ))}
+        </ul>
+    </main>
+  )
+}
+
+export default Content
